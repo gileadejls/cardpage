66719ad148b50d1c880c332eba5840232ce7cc5e
Gileadejls <gileadelima16@gmail.com>
2023-11-06 10:40:25 -0300

    atualizando

---
+++ card-website/script.js
@@ -0,0 +1,4 @@
+const nome = document.querySelector("#name")
+const numbercard = document.querySelector("#numbercard")
+const numberInput = document.querySelector("#cardnumber")
+const nometext = document.querySelector("#nametext")
